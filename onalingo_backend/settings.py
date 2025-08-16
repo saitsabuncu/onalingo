987cd245d963fe55ce2823f8dd646574702e3862
sait <sait@example.com>
2025-08-16 09:20:49 +0300

    en son ayarlar

--- onalingo_backend/settings.py
+++ onalingo_backend/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'lessons',
+    
 ]
+INSTALLED_APPS += ["corsheaders"]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'onalingo_backend.urls'
